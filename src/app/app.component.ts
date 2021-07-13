744006cf4ddfb373122664b702160e80d6b99c6e
Karla <karlaloya08@hotmail.com>
2021-07-12 19:58:34 -0500

    Cambios dress

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,45 +1,55 @@
 import { Component, OnInit, AfterViewInit } from '@angular/core';
 import { Web3Service } from './services/web3.service';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent implements AfterViewInit {
-  title = 'Ejemplo Ethereum';
-  estado = 'No Conectado.';
+  cuenta = '';
+  title = 'K Dresses';
+  estado = 'No Conectado';
   count = 0;
   resultado = '';
-
   blockHash = '';
   blockNumber = '';
   from = '';
+  to = '';
   transactionHash = '';
 
   elementos: any = [];
-  cabeceras = ['Transaction Hash', 'Block Number','Valor'];
+  cabeceras = ['Enviado por', 'Enviado a', 'Cantidad','Transaction Hash', 'Block Number'];
 
   constructor(public web3s: Web3Service){
   }
 
   ngAfterViewInit(): void {
     this.web3s.connectAccount().then((r)=>{ 
-                                    this.estado = "Conectado.";
+                                    this.estado = "Conectado";
                                     this.subscribeToEvents();
                                   });
   }
 
-  getCount(): void {
-    this.web3s.contrato.methods.getCount().call().then((response: any) => {
+  limpiar(): void {
+    this.cuenta = "";
+    this.resultado = "";
+    this.blockNumber = "";
+    this.transactionHash = "";
+    this.from = "";
+  }
+
+  balanceOf(): void {
+    this.web3s.contrato.methods.balanceOf(this.web3s.accounts[0]).call().then((response: any) => {
                                 this.count = response;
                                                        });
   }
 
-  increment(): void {
-    this.web3s.contrato.methods.increment().send({from: this.web3s.accounts[0]})
+  transfer(): void {
+    this.web3s.contrato.methods.transfer(this.cuenta,1).send({from: this.web3s.accounts[0]})
                                            .then((response:any) => {
-                                              this.resultado = "Transacción realizada!";
-                                              
+                                              this.resultado = "Envío realizado!";
                                               this.blockHash = response.blockHash;
                                               this.blockNumber = response.blockNumber;
                                               this.from = response.from;
@@ -47,23 +57,25 @@ export class AppComponent implements AfterViewInit {
                                            })
                                            .catch((error: any) => {
                                               console.log(error);
-                                              this.resultado = "Error en la transacción!";
+                                              this.resultado = "Error al enviar!";
                                            });
   }
 
   subscribeToEvents() {
     // Subscribe to pending transactions
     const self = this;
-    this.web3s.contrato.events.ValueChanged({
+    this.web3s.contrato.events.Transfer({
                                               fromBlock: 0
                                             },
                                             (error: any, event: any) => {
                                               if (!error){                                                        
                                                 this.elementos.push(
                                                   { blockHash: event.blockHash,
-                                                    transactionHash: event.transactionHash,
-                                                    blockNumber:event.blockNumber,                                             
-                                                    valor: event.returnValues.newValue
+                                                    blockNumber:event.blockNumber,
+                                                    transactionHash: event.transactionHash,                                 
+                                                    from: event.returnValues.from,
+                                                    to: event.returnValues.to,
+                                                    cant: event.returnValues.tokens
                                                   }
                                                 );                                                
                                               }                                              
